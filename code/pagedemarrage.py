955b1b37b2528ad92f205aede7023c54fa4de803
matteo <matteolap34400@gmail.com>
2026-04-13 09:57:36 +0200

    debut pagz accueil

--- code/pagedemarrage.py
+++ code/pagedemarrage.py
@@ -6,6 +6,7 @@ def ecran_demarrage(surface, WIDTH, HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, screen,
     font_title = pygame.font.Font(None, 80)
     font_text = pygame.font.Font(None, 40)
     font_small = pygame.font.Font(None, 30)
+    font_fonct = pygame.font.Font(None, 25)
     surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # --- Logo carré ---
@@ -22,6 +23,10 @@ def ecran_demarrage(surface, WIDTH, HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, screen,
 
     running_start = True
     while running_start:
+        ### PARTIE AFFICHAGE JEU###
+
+
+
         surface.fill((50, 150, 200))
         surface.blit(carre, (160,60))
 
@@ -29,6 +34,20 @@ def ecran_demarrage(surface, WIDTH, HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, screen,
         titre = font_title.render("Décheterie Simulator", True, (255, 255, 255))
         surface.blit(titre, (WIDTH//2 - titre.get_width()//2, HEIGHT//5))
 
+        fonctionnement = font_fonct.render("Vous incarnez un agent qui découvre la nouvelle déchetterie de Telleville, gérée par Lundi Agglo.",True,(255, 255, 255))
+        surface.blit(fonctionnement, (50 , 300 - fonctionnement.get_height()//2))
+
+        fonctionnement2 = font_fonct.render("Votre objectif est d’aider les visiteurs à trier correctement leurs déchets. Pour cela :",True,(255, 255, 255))
+        surface.blit(fonctionnement2, (50 , 325 - fonctionnement2.get_height()//2))
+
+        fonctionnement3 = font_fonct.render("- Vous devrez vider les voitures des visiteurs.",True,(255, 255, 255))
+        surface.blit(fonctionnement3, (70 , 350 - fonctionnement3.get_height()//2))
+
+        fonctionnement4 = font_fonct.render("- Placer les déchets dans les bonnes bennes rapporte des points.",True,(255, 255, 255))
+        surface.blit(fonctionnement4, (70 , 375 - fonctionnement4.get_height()//2))
+
+        fonctionnement5 = font_fonct.render("- Se tromper vous fait perdre des points.",True,(255, 255, 255))
+        surface.blit(fonctionnement5, (70 , 400 - fonctionnement5.get_height()//2))
         if etape == 1:
             texte = font_text.render("Choisissez la difficulté :", True, (255, 255, 255))
             surface.blit(texte, (WIDTH//2 - texte.get_width()//2, HEIGHT//2 - 60))
