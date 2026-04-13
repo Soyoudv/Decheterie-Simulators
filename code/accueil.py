6ce6133857342f2bb1c15d7c1391aa649c43f3e5
matteo <matteolap34400@gmail.com>
2026-04-13 12:22:13 +0200

    correction des positions des textes sur a l'ajout de la police

--- code/accueil.py
+++ code/accueil.py
@@ -3,9 +3,8 @@
 import pygame, sys
 
 def ecran_accueil(WIDTH, HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, screen, clock):
-    font_title = pygame.font.Font(None, 80)
-    font_fonct = pygame.font.Font(None, 35)
-    
+    font_title = pygame.font.Font("fonts/arial.ttf", 80)
+    font_fonct = pygame.font.Font("fonts/arial.ttf", 30)
 
     game_surface = pygame.Surface((WIDTH, HEIGHT))
 
@@ -30,16 +29,16 @@ def ecran_accueil(WIDTH, HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, screen, clock):
         game_surface.blit(fonctionnement, (50 , 500 - fonctionnement.get_height()//2))
 
         fonctionnement2 = font_fonct.render("Votre objectif est d’aider les visiteurs à trier correctement leurs déchets. Pour cela :",True,(255, 255, 255))
-        game_surface.blit(fonctionnement2, (50 , 525 - fonctionnement2.get_height()//2))
+        game_surface.blit(fonctionnement2, (50 , 535 - fonctionnement2.get_height()//2))
 
         fonctionnement3 = font_fonct.render("- Vous devrez vider les voitures des visiteurs.",True,(255, 255, 255))
-        game_surface.blit(fonctionnement3, (70 , 550 - fonctionnement3.get_height()//2))
+        game_surface.blit(fonctionnement3, (70 , 570 - fonctionnement3.get_height()//2))
 
         fonctionnement4 = font_fonct.render("- Placer les déchets dans les bonnes bennes rapporte des points.",True,(255, 255, 255))
-        game_surface.blit(fonctionnement4, (70 , 575 - fonctionnement4.get_height()//2))
+        game_surface.blit(fonctionnement4, (70 , 605 - fonctionnement4.get_height()//2))
 
         fonctionnement5 = font_fonct.render("- Se tromper vous fait perdre des points.",True,(255, 255, 255))
-        game_surface.blit(fonctionnement5, (70 , 600 - fonctionnement5.get_height()//2))
+        game_surface.blit(fonctionnement5, (70 , 640 - fonctionnement5.get_height()//2))
 
         # souris en coords logiques
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -82,4 +81,5 @@ def ecran_accueil(WIDTH, HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, screen, clock):
         screen.blit(scaled, (0, 0))
 
         pygame.display.flip()
+
         clock.tick(60)
